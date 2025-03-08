bb10e6a9b5741d353c33e9c4842affdb5cf2e253
jnollen13 <jerome.nollen@gmail.com>
2025-03-08 18:47:07 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,37 @@
 namespace SpriteKind {
     export const coinlvl3 = SpriteKind.create()
+    export const bird = SpriteKind.create()
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.splash("no cheating", "or skips for you")
+    game.reset()
+})
 function jumping () {
-    if (jump == 0 || jump == 1) {
-        mySprite.vy = randint(-48, -57)
-        if (jump == 1) {
-            mySprite.startEffect(effects.trail, randint(222, 777))
+    if (start == 0) {
+        if (jump == 0 || jump == 1) {
+            mySprite.vy = randint(-48, -59)
+            if (jump == 1) {
+                mySprite.startEffect(effects.trail, randint(222, 777))
+            }
+            jump += 1
         }
-        jump += 1
     }
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     jumping()
 })
+controller.combos.attachCombo("U+DU+DBBA", function () {
+    lvl += 1
+    game.splash("U+DU+DBBA", "developer button combo")
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile15`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        mySprite.vy = 0
+        jump = 0
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     jumping()
-})
-controller.combos.attachCombo("DDBB", function () {
-    lvl += 1
-    game.splash("DD+BB")
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
     tiles.setTileAt(tiles.getTileLocation(11, 25), assets.tile`myTile`)
@@ -37,15 +50,15 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, l
     lvl += 1
     if (lvl == 1) {
         game.showLongText("lvl 1 complete", DialogLayout.Full)
-        tiles.setCurrentTilemap(tilemap`level0`)
+        tiles.setCurrentTilemap(tilemap`level02`)
         tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 30))
     } else if (lvl == 2) {
         game.showLongText("lvl 2 complete", DialogLayout.Full)
-        tiles.setCurrentTilemap(tilemap`level8`)
+        tiles.setCurrentTilemap(tilemap`level0`)
         tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 18))
     } else if (lvl == 3) {
         game.showLongText("lvl 3 complete", DialogLayout.Full)
-        tiles.setCurrentTilemap(tilemap`level11`)
+        tiles.setCurrentTilemap(tilemap`level3`)
         tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 15))
         mySprite2 = sprites.create(assets.image`coin`, SpriteKind.coinlvl3)
         tiles.placeOnTile(mySprite2, tiles.getTileLocation(32, 6))
@@ -98,12 +111,45 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, l
         mySprite12.setBounceOnWall(true)
         mySprite13.setBounceOnWall(true)
         mySprite14.setBounceOnWall(true)
-        mySprite3.setVelocity(0, 15)
-        mySprite2.setVelocity(0, 25)
-        mySprite4.setVelocity(0, 30)
-        mySprite5.setVelocity(0, 40)
-        mySprite6.setVelocity(0, 50)
-        mySprite7.setVelocity(0, 55)
+        mySprite3.setVelocity(0, 10)
+        mySprite2.setVelocity(0, 15)
+        mySprite4.setVelocity(0, 20)
+        mySprite5.setVelocity(0, 25)
+        mySprite6.setVelocity(0, 30)
+        mySprite7.setVelocity(0, 35)
+        mySprite8.setVelocity(0, 40)
+        mySprite9.setVelocity(0, 45)
+        mySprite10.setVelocity(0, 50)
+        mySprite11.setVelocity(0, 55)
+        mySprite12.setVelocity(0, 60)
+        mySprite13.setVelocity(0, 65)
+        mySprite14.setVelocity(0, 10)
+    } else if (lvl == 5) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        game.showLongText("Lvl 5 complete", DialogLayout.Full)
+        tiles.setCurrentTilemap(tilemap`level6`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 14))
+    } else if (lvl == 6) {
+        game.showLongText("lvl 6 complete", DialogLayout.Full)
+        tiles.setCurrentTilemap(tilemap`level8`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 14))
+    } else if (lvl == 7) {
+        game.showLongText("lvl 7 complete", DialogLayout.Full)
+        tiles.setCurrentTilemap(tilemap`level11`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 14))
+    } else if (lvl == 8) {
+        game.showLongText("Lvl 8 complete", DialogLayout.Full)
+        tiles.setCurrentTilemap(tilemap`level15`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 168))
+        birdtype = randint(1, 2)
+        bird = sprites.create(assets.image`birdL`, SpriteKind.bird)
+        tiles.placeOnTile(bird, tiles.getTileLocation(15, 116))
+        bird.setVelocity(-75, 0)
+        go = 1
+        bird.setBounceOnWall(true)
+    } else if (lvl == 9) {
+        tiles.setCurrentTilemap(tilemap`level18`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -118,6 +164,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, l
     mySprite.setVelocity(-999, 0)
     mySprite.x = -9999
 })
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += 21
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     game.setGameOverPlayable(false, music.melodyPlayable(music.spooky), false)
     game.setGameOverEffect(false, effects.melt)
@@ -126,27 +175,43 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     if (controller.up.isPressed()) {
-        mySprite.y += -5
+        mySprite.vy += -10
     }
 })
-controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setFlag(SpriteFlag.ShowPhysics, true)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.bird, function (sprite, otherSprite) {
+    if (birdtype == 1) {
+        jump = 0
+    } else if (birdtype == 2) {
+        if (gravityud == 0) {
+            gravityud = 1
+            mySprite.ay = -50
+            mySprite.y += -33
+        } else if (gravityud == 1) {
+            gravityud = 0
+            mySprite.ay = 50
+            mySprite.y += 33
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile14`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.coinlvl3, function (sprite, otherSprite) {
-    if (controller.A.isPressed()) {
-        sprites.destroy(mySprite2, effects.rings, 111)
-        tiles.setWallAt(tiles.getTileLocation(23, 7), false)
-        tiles.setTileAt(tiles.getTileLocation(23, 7), assets.tile`myTile0`)
-        tiles.setWallAt(tiles.getTileLocation(23, 8), false)
-        tiles.setTileAt(tiles.getTileLocation(23, 8), assets.tile`myTile0`)
-    }
+    sprites.destroy(mySprite2, effects.rings, 111)
+    tiles.setWallAt(tiles.getTileLocation(23, 7), false)
+    tiles.setTileAt(tiles.getTileLocation(23, 7), assets.tile`myTile0`)
+    tiles.setWallAt(tiles.getTileLocation(23, 8), false)
+    tiles.setTileAt(tiles.getTileLocation(23, 8), assets.tile`myTile0`)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.setGameOverMessage(false, "You were stabbed...")
-    game.setGameOverEffect(false, effects.slash)
-    game.gameOver(false)
+    game.splash("Congratulations!", "You were stabbed...")
+    if (lvl == 4) {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(31, 1))
+    }
 })
 let v2 = 0
+let bird: Sprite = null
+let birdtype = 0
 let mySprite14: Sprite = null
 let mySprite13: Sprite = null
 let mySprite12: Sprite = null
@@ -161,30 +226,59 @@ let mySprite4: Sprite = null
 let mySprite3: Sprite = null
 let mySprite2: Sprite = null
 let jump = 0
+let sound = 0
+let gravityud = 0
 let mySprite: Sprite = null
 let lvl = 0
+let go = 0
+let start = 0
+let speedrun = 0
+start = 1
+go = 0
 lvl = 0
-info.setScore(0)
 game.setDialogFrame(assets.image`df`)
 game.setDialogTextColor(1)
 game.setDialogCursor(assets.image`dc`)
 mySprite = sprites.create(assets.image`player1`, SpriteKind.Player)
 mySprite.setStayInScreen(true)
-mySprite.ay = 11
+mySprite.ay = 0
 mySprite.setBounceOnWall(false)
 scene.cameraFollowSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`level1`)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 14))
-controller.moveSprite(mySprite, 50, 0)
-game.onUpdate(function () {
-    info.setScore(game.runtime())
-})
+gravityud = 0
+game.splash("game options")
+story.showPlayerChoices("show physics", "don't show physics")
+if (story.getLastAnswer() == "show physics") {
+    mySprite.setFlag(SpriteFlag.ShowPhysics, true)
+} else {
+    mySprite.setFlag(SpriteFlag.ShowPhysics, false)
+    mySprite.setVelocity(0, 0)
+}
+story.showPlayerChoices("speed-running mode", "normal mode")
+if (story.getLastAnswer() == "normal mode") {
+    speedrun = 0
+} else if (story.checkLastAnswer("speed-running mode")) {
+    speedrun = 1
+}
+story.showPlayerChoices("sound on?", "Sound off?")
+if (story.checkLastAnswer("sound on?")) {
+    sound = 1
+} else if (story.checkLastAnswer("sound off?")) {
+    sound = 0
+}
+controller.moveSprite(mySprite, 53, 0)
+pause(100)
+mySprite.setVelocity(0, 0)
+start = 0
 game.onUpdate(function () {
     if (!(mySprite.isHittingTile(CollisionDirection.Bottom))) {
         animation.stopAnimation(animation.AnimationTypes.ImageAnimation, mySprite)
-        mySprite.ay = 50
         v2 = 1
         controller.moveSprite(mySprite, 31, 0)
+        if (gravityud == 0) {
+            mySprite.ay = 50
+        }
         if (jump == 0) {
             jump = 1
         }
@@ -195,5 +289,29 @@ game.onUpdate(function () {
         if (v2 == 1) {
             v2 = 0
         }
+    }
+})
+game.onUpdate(function () {
+    if (speedrun == 1) {
+        info.setScore(game.runtime())
+    }
+})
+game.onUpdate(function () {
+    if (controller.B.isPressed() && mySprite.tileKindAt(TileDirection.Left, assets.tile`myTile7`)) {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
+    }
+})
+game.onUpdate(function () {
+    if (go == 1) {
+        if (bird.isHittingTile(CollisionDirection.Left)) {
+            bird.setImage(assets.image`birdR`)
+        } else if (bird.isHittingTile(CollisionDirection.Right)) {
+            bird.setImage(assets.image`birdL`)
+        }
+    }
+})
+game.onUpdateInterval(60000, function () {
+    if (sound == 1 && Math.percentChance(12.2)) {
+        music.play(music.createSong(assets.song`song0`), music.PlaybackMode.UntilDone)
     }
 })
